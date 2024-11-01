26e3ffc3bb3df73ef568091e33be167857c5b360
Sourav Halder <bishalder96@gmail.com>
2024-11-01 15:54:25 +0530

    image & color constant file added

---
+++ lib/config/constants.dart
@@ -0,0 +1,12 @@
+// Image constants
+import 'package:flutter/material.dart';
+
+const String onboardingOne = 'assets/images/onboarding_1.png';
+const String onboardingTwo = 'assets/images/onboarding_2.png';
+const String onboardingThree = 'assets/images/onboarding_3.png';
+const String onboardingFour = 'assets/images/onboarding_4.png';
+
+// Color constants
+const Color primaryColor = Color(0xFFFFB939);
+const Color textColor = Color(0xFF000000);
+const Color descriptionColor = Color(0xFF404040);
