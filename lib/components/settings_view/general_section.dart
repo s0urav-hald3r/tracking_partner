88587d02d89f3f776591e7b40bb49e48fbecaac3
s0urav-hald3r <bishalder96@gmail.com>
2024-11-14 01:17:37 +0530

    in app review feature added

--- lib/components/settings_view/general_section.dart
+++ lib/components/settings_view/general_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_advanced_switch/flutter_advanced_switch.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:in_app_review/in_app_review.dart';
 import 'package:tracking_partner/config/constants.dart';
 import 'package:tracking_partner/controllers/settings_controller.dart';
 
@@ -15,6 +16,9 @@ class GeneralSection extends StatefulWidget {
 class _GeneralSectionState extends State<GeneralSection> {
   late final ValueNotifier<bool> _controller;
   final settingsController = SettingsController.instance;
+
+  // App review
+  final InAppReview inAppReview = InAppReview.instance;
 
   @override
   void initState() {
@@ -37,99 +41,98 @@ class _GeneralSectionState extends State<GeneralSection> {
         borderRadius: BorderRadius.circular(10.r),
         color: Colors.white,
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 32.w,
-                height: 32.h,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.r),
-                  color: const Color(0xFFF8F8F8),
-                ),
-                child: Center(
-                  child: SvgPicture.asset(bell),
-                ),
-              ),
-              SizedBox(width: 10.w),
-              Text(
-                'Notification',
-                style: TextStyle(
-                  fontSize: 12.sp,
-                  color: textColor,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              const Spacer(),
-              AdvancedSwitch(
-                controller: _controller,
-                initialValue: settingsController.isNotificationEnable,
-                activeColor: Colors.green,
-                inactiveColor: Colors.grey,
-                activeChild: const Text('I'),
-                inactiveChild: const Text('O'),
-                width: 36.w,
-                height: 18.h,
-                enabled: true,
-              ),
-            ],
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Row(children: [
+          Container(
+            width: 32.w,
+            height: 32.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.r),
+              color: const Color(0xFFF8F8F8),
+            ),
+            child: Center(
+              child: SvgPicture.asset(bell),
+            ),
           ),
-          Row(
-            children: [
-              Container(
-                width: 32.w,
-                height: 32.h,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.r),
-                  color: const Color(0xFFF8F8F8),
-                ),
-                child: Center(
-                  child: SvgPicture.asset(share),
-                ),
-              ),
-              SizedBox(width: 10.w),
-              Text(
-                'Share the app',
-                style: TextStyle(
-                  fontSize: 12.sp,
-                  color: textColor,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              const Spacer(),
-              Icon(Icons.chevron_right, size: 18.sp)
-            ],
+          SizedBox(width: 10.w),
+          Text(
+            'Notification',
+            style: TextStyle(
+              fontSize: 12.sp,
+              color: textColor,
+              fontWeight: FontWeight.w500,
+            ),
           ),
-          Row(
-            children: [
-              Container(
-                width: 32.w,
-                height: 32.h,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.r),
-                  color: const Color(0xFFF8F8F8),
-                ),
-                child: Center(
-                  child: SvgPicture.asset(feedback),
-                ),
-              ),
-              SizedBox(width: 10.w),
-              Text(
-                'Feedback',
-                style: TextStyle(
-                  fontSize: 12.sp,
-                  color: textColor,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              const Spacer(),
-              Icon(Icons.chevron_right, size: 18.sp)
-            ],
+          const Spacer(),
+          AdvancedSwitch(
+            controller: _controller,
+            initialValue: settingsController.isNotificationEnable,
+            activeColor: Colors.green,
+            inactiveColor: Colors.grey,
+            activeChild: const Text('I'),
+            inactiveChild: const Text('O'),
+            width: 36.w,
+            height: 18.h,
+            enabled: true,
           ),
-        ],
-      ),
+        ]),
+        Row(children: [
+          Container(
+            width: 32.w,
+            height: 32.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.r),
+              color: const Color(0xFFF8F8F8),
+            ),
+            child: Center(
+              child: SvgPicture.asset(share),
+            ),
+          ),
+          SizedBox(width: 10.w),
+          Text(
+            'Share the app',
+            style: TextStyle(
+              fontSize: 12.sp,
+              color: textColor,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const Spacer(),
+          Icon(Icons.chevron_right, size: 18.sp)
+        ]),
+        InkWell(
+          onTap: () async {
+            if (await inAppReview.isAvailable()) {
+              inAppReview.requestReview();
+            }
+          },
+          child: Row(children: [
+            Container(
+              width: 32.w,
+              height: 32.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.r),
+                color: const Color(0xFFF8F8F8),
+              ),
+              child: Center(
+                child: SvgPicture.asset(feedback),
+              ),
+            ),
+            SizedBox(width: 10.w),
+            Text(
+              'Feedback',
+              style: TextStyle(
+                fontSize: 12.sp,
+                color: textColor,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const Spacer(),
+            Icon(Icons.chevron_right, size: 18.sp)
+          ]),
+        ),
+      ]),
     );
   }
 }
