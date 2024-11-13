960fcc57a1b97d9d948a282660f779846a00163f
s0urav-hald3r <bishalder96@gmail.com>
2024-11-14 01:25:09 +0530

    urls attach on links on purchase view

--- lib/components/purchase_view/purchase_links.dart
+++ lib/components/purchase_view/purchase_links.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tracking_partner/config/constants.dart';
 import 'package:tracking_partner/controllers/settings_controller.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PurchaseLinks extends StatelessWidget {
   const PurchaseLinks({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text(
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      InkWell(
+        onTap: () async {
+          Uri uri = Uri.parse(termsCondition);
+          if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+            throw Exception('Could not launch $uri');
+          }
+        },
+        child: Text(
           'Terms of Use',
           style: TextStyle(
             fontSize: 12.sp,
@@ -20,19 +26,27 @@ class PurchaseLinks extends StatelessWidget {
             decoration: TextDecoration.underline,
           ),
         ),
-        InkWell(
-          onTap: () => SettingsController.instance.restorePurchases(),
-          child: Text(
-            'Restore Purchase',
-            style: TextStyle(
-              fontSize: 12.sp,
-              fontWeight: FontWeight.w400,
-              color: greyColor,
-              decoration: TextDecoration.underline,
-            ),
+      ),
+      InkWell(
+        onTap: () => SettingsController.instance.restorePurchases(),
+        child: Text(
+          'Restore Purchase',
+          style: TextStyle(
+            fontSize: 12.sp,
+            fontWeight: FontWeight.w400,
+            color: greyColor,
+            decoration: TextDecoration.underline,
           ),
         ),
-        Text(
+      ),
+      InkWell(
+        onTap: () async {
+          Uri uri = Uri.parse(privacyPolicy);
+          if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+            throw Exception('Could not launch $uri');
+          }
+        },
+        child: Text(
           'Privacy Policy',
           style: TextStyle(
             fontSize: 12.sp,
@@ -41,7 +55,7 @@ class PurchaseLinks extends StatelessWidget {
             decoration: TextDecoration.underline,
           ),
         ),
-      ],
-    );
+      ),
+    ]);
   }
 }
